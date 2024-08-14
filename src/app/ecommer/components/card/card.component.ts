5760a414ae8838377a5ea9e19c5ec20c181d46cf
jose angel <jose.albelt1817@gmail.com>
2024-08-14 17:42:57 -0500

    new ecommer page

--- src/app/ecommer/components/card/card.component.ts
+++ src/app/ecommer/components/card/card.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+interface Product {
+  id: number;
+  image: string;
+  title: string;
+  price: number;
+  rank: number;
+  selected: boolean;
+}
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -7,4 +16,57 @@ import { Component } from '@angular/core';
 })
 export class CardComponent {
 
+  products: Product[] = [
+    {
+      id: 1,
+      image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+      title: 'PRODUCT',
+      price: 200,
+      rank: 1.2,
+      selected: false
+    },
+    {
+      id: 2,
+      image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+      title: 'PRODUCT 2',
+      price: 300,
+      rank: 4.5,
+      selected: false
+    },
+ 
+  ];
+  
+ public productSelect: any[] = [];
+  
+  onToggleSelect(product:Product) {
+    product.selected = !product.selected;
+    this.updateToggleSelect(product)
+  }
+
+  updateToggleSelect(product:Product){
+      if(product.selected){
+          this.productSelect.push(product);
+      }else {
+        this.productSelect = this.productSelect.filter(p => p.id !== product.id);
+      }
+      console.log(this.productSelect);
+  }
+
+  
+
+  getStarRating(rank: number): any {
+    const fullStars = Math.floor(rank);
+    const halfStars = rank % 1 >= 0.5 ? 1 : 0;
+    const emptyStars = 5 - fullStars - halfStars;
+
+    return [
+      ...Array(fullStars).fill('full'),
+      ...Array(halfStars).fill('half'),
+    ];
+  }
+
+  buyProductSelect(product:Product){
+      console.log('comprar ',product)
+  }
+    
 }
