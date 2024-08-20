d606a2c86453342dc51bbed734c8901da2019bf3
jose angel <jose.albelt1817@gmail.com>
2024-08-20 12:24:40 -0500

    register page

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
@@ -6,5 +8,57 @@ import { Component } from '@angular/core';
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
+
+
+  public myForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+    
+    this.myForm = this.fb.group({
+      email:['',[Validators.required, Validators.email]],
+      password:['',[Validators.required]],
+      password2:['',[Validators.required]],
+    });
+
+  }
+
+  isValidField( field:string, ){
+    return this.myForm.controls[field].errors 
+      &&  this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field:string): string| null{
+    if( !this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for( const key of Object.keys(errors) ){
+      switch(key){
+        case 'required':
+          return 'este campo es requerido';
+        case 'email':
+           return `el formato de correo no corresponde`;
+      }
+    }
+
+    return '';
+  }
+     
+  onSave() {
+    if(this.myForm.valid){
+      return this.router.navigate(['/ecommer/eco']);
+    }
+
+    return 
+  }
+
+  registrer() {
+    this.router.navigate(['/auth/new-account']);
+  }
+
+
 
 }
