de49fd1e31ba7b5eb98a6caace5c6c0cfbe3cc1e
jose angel <jose.albelt1817@gmail.com>
2024-08-30 11:48:13 -0500

    structure and menu layout

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layoutPage/layoutPage.component';
+import { AddNewProductComponent } from './pages/add-new-product/add-new-product.component';
+import { ListProductComponent } from './pages/list-product/list-product.component';
+import { ProductVisualComponent } from './pages/product-visual/product-visual.component';
 
 const routes: Routes = [
 
   {
     path:'',
     component:LayoutPageComponent,
-    children:[]
+    children:[
+      {path:'addProduct',component:AddNewProductComponent},
+      {path:'listProduct',component:ListProductComponent},
+      {path:':id',component:ProductVisualComponent},
+      {path:'**',redirectTo:'addProduct'}
+    ]
   }
 
 ];
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { AddNewProductComponent } from './pages/add-new-product/add-new-product.component';
+import { ProductVisualComponent } from './pages/product-visual/product-visual.component';
+import { ListProductComponent } from './pages/list-product/list-product.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddNewProductComponent,
+    ProductVisualComponent,
+    ListProductComponent
+  ],
   imports: [
     CommonModule,
     DashboardRoutingModule
