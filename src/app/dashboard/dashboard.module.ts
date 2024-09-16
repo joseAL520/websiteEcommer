6bc34628d839105e44c14640155386ee2a4fde62
jose angel <jose.albelt1817@gmail.com>
2024-09-16 10:19:42 -0500

    add new product page

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,17 +5,20 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { AddNewProductComponent } from './pages/add-new-product/add-new-product.component';
 import { ProductVisualComponent } from './pages/product-visual/product-visual.component';
 import { ListProductComponent } from './pages/list-product/list-product.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AddNewProductComponent,
     ProductVisualComponent,
-    ListProductComponent
+    ListProductComponent,
+
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    ReactiveFormsModule,    
   ]
 })
 export class DashboardModule { }
--- src/app/dashboard/pages/add-new-product/add-new-product.component.ts
+++ src/app/dashboard/pages/add-new-product/add-new-product.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Category, Products } from '../../interfaces/product.interfaces';
 
 @Component({
   selector: 'app-add-new-product',
@@ -6,5 +8,64 @@ import { Component } from '@angular/core';
   styleUrl: './add-new-product.component.css'
 })
 export class AddNewProductComponent {
+  
+  public productForm: FormGroup;
+
+  public category =[
+    {id: 'Electronics',  name: "electronics"},
+    {id: 'Jewelery',  name: "jewelery"},
+    {id: 'MenSClothing',  name: "men's clothing"},
+    {id: 'WomenSClothing',  name: "women's clothing"},
+  ];
+
+  public imagePreview: string | ArrayBuffer | null = null;
+
+ constructor(private fb: FormBuilder) {
+    this.productForm = this.fb.group({
+      // id: [0, [Validators.required]],
+      title: ['', [Validators.required]],
+      price: [0, [Validators.required,Validators.min(1)]],
+      category: ['', [Validators.required]],
+      count: [0, [Validators.required,Validators.min(1)]],
+      img: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+    });
+
+    this.productForm.get('img')?.valueChanges.subscribe(value => {
+      this.imagePreview = value;
+    });
+  }
+
+  isValidField( field:string, ){
+     return this.productForm.controls[field].errors &&  this.productForm.controls[field].touched;
+  }
+
+  getFieldError(field:string): string| null{
+    if( !this.productForm.controls[field]) return null;
+
+    const errors = this.productForm.controls[field].errors || {};
+
+    for( const key of Object.keys(errors) ){
+      switch(key){
+        case 'required':
+          return 'este campo es requerido';
+        case 'minlength':
+          return `Minimo ${ errors['minlength'].requiredLength } `;
+        case 'min':
+          return 'Las existencias deben de ser mayor 0'
+      }
+    }
+
+    return '';
+  }
+
+  
+
+  onSubmit() {
+      
+
+    console.log(this.productForm.value)
+
+  }
 
 }
