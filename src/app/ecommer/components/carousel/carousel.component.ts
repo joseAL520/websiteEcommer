3aa94295899fd4f4af8a0d84e82b79cbc1686590
jose angel <jose.albelt1817@gmail.com>
2024-10-01 15:00:39 -0500

    carousel

--- src/app/ecommer/components/carousel/carousel.component.ts
+++ src/app/ecommer/components/carousel/carousel.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductServiceService } from '../../../dashboard/services/product-service.service';
+import { Products } from '../../../dashboard/interfaces/product.interfaces';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.css'
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit{
+
+  public productsRulet: Products[] = [];
+
+  constructor(
+    private productService: ProductServiceService
+  ){}
+
+
+  productPages(arg0: number|undefined) {
+    console.log(arg0)
+  }
+
+  
+  ngOnInit(): void {
+    this.productService.getProducts().pipe(
+      tap(Valu => {
+        Valu.sort( () => Math.random() - 0.5)
+      })
+    ).subscribe( randomProducts  => 
+       this.productsRulet = randomProducts
+    )
+  }
+
+
 
 }
